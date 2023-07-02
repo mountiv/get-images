b876d26c4ebfd617728943a645db8a7193b8b2ea
mountiv <mountiv.solution@outlook.com>
2023-07-02 18:03:50 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import sys
+
+def download_images(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Create a directory to store the images
+    if not os.path.exists('images'):
+        os.makedirs('images')
+
+    # Find all image tags on the page
+    img_tags = soup.find_all('img')
+
+    # Download each image
+    for img in img_tags:
+        img_url = img.get('src')
+        if img_url:
+            if img_url.startswith('http'):
+                img_response = requests.get(img_url)
+            else:
+                img_response = requests.get(url + img_url)
+
+            # Extract the filename from the URL
+            filename = img_url.split('/')[-1]
+
+            # Save the image to the 'images' directory
+            with open(os.path.join('images', filename), 'wb') as f:
+                f.write(img_response.content)
+
+            print(f'Downloaded {filename}')
+        else:
+            print('Image source URL not found.')
+
+url = sys.argv[1]
+print("URL:", url)
+
+download_images(url)
